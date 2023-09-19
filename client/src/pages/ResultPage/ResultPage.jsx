c9988cd11826d8e1d1ead0f63b59d5940f4f1bbe
vegam91 <vegamartinoj@gmail.com>
2023-09-19 17:19:19 +0200

    reajust style resultpage

--- client/src/pages/ResultPage/ResultPage.jsx
+++ client/src/pages/ResultPage/ResultPage.jsx
@@ -1,56 +1,73 @@
 import { Container, Stack, Box, Button, Typography } from "@mui/material";
-import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
 import { Chart } from "chart.js";
-// import Style from "./Style"
+// import Style from "./Style";
+import React, { useState } from "react";
+
 function ResultPage() {
   return (
     // <Style.MainContainer id="main-container">
-    <Container id= "content-container"
-      sx={{
-       backgroundColor: "9ED2BE",
-        border: "5px solid red",
-        display: "flex",
-        width: "100vw",
-        height: "100vh",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-      }}
+    <Stack
+      border={"10px solid red"}
+      display={"flex"}
+      width={"100vw"}
+      height={"100vh"}
+      flexDirection={"column"}
+      justifyContent={"flex-start"}
     >
-      <Stack id= "logoStack"
+      <Stack
+        id="logoStack"
         height="6vh"
-        border={"5px solid black"}
+        border={"5px solid green"}
         direction={"row"}
         alignItems={"center"}
       >
         LOGO
       </Stack>
       <Stack
-        // border="5px solid blue"
+        
+        id="buttonStack"
+        border="5px solid blue"
         display="flex"
         height="5vh"
         flexDirection="row"
         alignItems="center"
         justifyContent={"center"}
       >
-        <Button style={{ borderRadius: '12px' }} variant="contained">Contained</Button>
-        <Button style={{ borderRadius: '12px' }}variant="outlined">Outlined</Button>
+        <Button
+          style={{ borderRadius: "12px" }}
+          variant="contained"
+          onClick={() => handleButtonClick}
+        >
+          {" "}
+          Perfil cultural
+        </Button>
+        <Button style={{ borderRadius: "12px" }} variant="outlined">
+          KPIS de engagement
+        </Button>
       </Stack>
       <Stack
-        border="5px solid red"
+      marginTop={"20px"}
+      gap={"10px"}
+        border="5px solid pink"
         alignContent={"center"}
         justifyContent={"space-between"}
         flexDirection={"row"}
         height="40vh"
       >
-        <Box border={"5px solid blue"} justifyContent={"center"} alignContent={"center"} flex={1}>
-        <Typography> Ajuste cultural</Typography>
-          </Box>
-        <Box border={"5px solid blue"} flex={1}>
-          <Typography> KPIS</Typography>
-         
+        <Box
+          border={"5px solid blue"}
+          justifyContent={"center"}
+          alignContent={"center"}
+          flex={1}
+        >
+          <Typography> Ajuste cultural</Typography>
         </Box>
         <Box border={"5px solid blue"} flex={1}>
-        <Typography> Ajuste cultural deseado </Typography>
+          <Typography> KPIS</Typography>
+        </Box>
+        <Box border={"5px solid blue"} flex={1}>
+          <Typography> Ajuste cultural deseado </Typography>
         </Box>
       </Stack>
       <Stack
@@ -66,14 +83,18 @@ function ResultPage() {
         // border="5px solid blue"
         display="flex"
         height="5vh"
-       
         alignItems="center"
         justifyContent={"center"}
       >
-        <Button style={{ borderRadius: '12px' }} variant="contained"  startIcon={< DownloadRoundedIcon />}>Descarga tus resultados</Button>
-      
+        <Button
+          style={{ borderRadius: "12px" }}
+          variant="contained"
+          startIcon={<DownloadRoundedIcon />}
+        >
+          Descarga tus resultados
+        </Button>
       </Stack>
-    </Container>
+    </Stack>
     // </Style.MainContainer>
   );
 }
--- client/src/pages/ResultPage/Style.jsx
+++ client/src/pages/ResultPage/Style.jsx
@@ -1,5 +1,36 @@
-// import { styled } from "@mui/material"
+// import { styled } from "@mui/material";
 
-// const MainContainer = styled("div"){
-    
-// }
+// const MainContainer = styled("div")({
+//   border: "5px solid red",
+//   display: "flex",
+//   width: "100vw",
+//   height: "100vh",
+//   flexDirection: "column",
+//   justifyContent: "flex-start",
+
+//   "#content-container": {
+//     display: "flex",
+//     border: "10px solid black",
+//     height: "100%",
+//     flexDirection: "column",
+
+//     justifyContent: "space-between",
+//     gap: "24px",
+//   },
+
+//   logoStack: {
+//     height: "6vh",
+//     border: "5px solid black",
+//     direction: "row",
+//     alignItems: "center",
+//   },
+//   buttonStack: {
+//     border: "5px solid blue",
+//     display: "flex",
+//     height: "5vh",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+// export default { MainContainer };
