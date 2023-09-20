a4ae9f80f5fc492d49660fb69d0fd66a49b8852b
carlosaj98 <carlosaj.arahal@gmail.com>
2023-09-20 18:20:44 +0200

    update: improvement landing page design

--- client/src/pages/LandingPage/Style.jsx
+++ client/src/pages/LandingPage/Style.jsx
@@ -3,64 +3,80 @@ import { styled } from "@mui/material"
 const MainContainer = styled("div")({
   width: "100vw",
   height: "100vh",
-  backgroundImage: "url(/bg_landingPage.png)",
+  backgroundColor: "white",
+  backgroundImage: "url(/wave.png)",
   backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  color: "black",
+  backgroundSize:"cover",
 
-  "#content-container":{
+
+  "#content-container": {
     display: "flex",
     height: "100%",
     flexDirection: "column",
     flexWrap: "wrap",
-    justifyContent: "space-between",
+    alignItems: "center",
+    justifyContent:"space-between",
     gap: "24px",
   },
 
-  main:{
+  main: {
     padding: "64px",
     display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    gap: "24px"
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 
   h1: {
     marginTop: "100px",
-    fontSize: "4rem",
+    fontSize: "5rem",
     fontWeight: "bold",
-    color: "#00C3C0"
+    color: "var(--primary-color)",
+    fontFamily:"var(--secondary-font)",
   },
 
   "#title-description": {
-      textAlign: "left",
-      fontSize: "1.3rem",
-      maxWidth: "700px",
-      marginBottom: "24px"
+    fontFamily:"var(--primary-font)",
+    textAlign: "left",
+    fontSize: "1.5rem",
+    maxWidth: "800px",
+    marginBottom: "24px",
   },
 
-  "#content-container button":{
-    backgroundColor: "#00C3C0",
+  "#content-container button": {
+    backgroundColor: "var(--primary-color)",
     borderRadius: "25px",
     padding: "18px 100px",
+    fontFamily:"var(--primary-font)",
     fontSize: "1.15rem",
   },
 
-  footer:{
+  footer: {
     width: "100%",
     height: "fit-content",
     display: "flex",
     justifyContent: "center",
     gap: "16px",
     padding: "6px",
-    backgroundColor: "#00C3C0",
   },
 
-  "footer p":{
+  "footer p": {
     color: "white",
     fontWeight: "bold",
   },
 
+  "footer a": {
+    color: "white",
+    fontFamily: "var(--primary-font)",
+  },
+
+  "#logo":{
+    width: "400px",
+    height: "400px",
+    backgroundImage: "url(/logoCF.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize:"cover",
+  }
 })
 
 export default { MainContainer }
