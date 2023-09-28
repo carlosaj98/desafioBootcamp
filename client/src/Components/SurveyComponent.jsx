68997cab62eda8666ceade267b610e340b179309
Kilox6991 <4inf.mpromero@gmail.com>
2023-09-28 21:43:05 +0200

    Modal instrucciones

--- client/src/Components/SurveyComponent.jsx
+++ client/src/Components/SurveyComponent.jsx
@@ -6,11 +6,12 @@ import "survey-core/defaultV2.min.css";
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import Modal from "@mui/material/Modal";
+import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { themeJson } from "../theme";
 import { useNavigate } from "react-router-dom";
-
-import { isScreenWideEnough } from "../Data/json"
+import { isScreenWideEnough } from "../Data/json";
 
 function SurveyComponent({
   data,
@@ -22,25 +23,19 @@ function SurveyComponent({
 }) {
   const survey = new Model(data);
 
-  const stepData = localStorage.getItem('allResults')?.split('').slice(activeStep * 6, activeStep * 6 + 6)
+  const stepData = localStorage
+    .getItem("allResults")
+    ?.split("")
+    .slice(activeStep * 6, activeStep * 6 + 6);
 
   if (stepData) {
     survey.data = {
-      [data.elements[activeStep]]: [
-        1,
-        2,
-        6,
-        5,
-        4,
-        3
-      ]
-    }
+      [data.elements[activeStep]]: [1, 2, 6, 5, 4, 3],
+    };
   }
 
   const theme = useTheme();
   const navigate = useNavigate();
-
-  // You can delete the line below if you do not use a customized theme
 
   survey.applyTheme(themeJson);
   survey.onComplete.add((sender, options) => {
@@ -49,7 +44,7 @@ function SurveyComponent({
   });
 
   survey.onValueChanged.add((sender, options) => {
-    if (!isScreenWideEnough()) return
+    if (!isScreenWideEnough()) return;
 
     const container = document.querySelector(
       ".sv-ranking__container.sv-ranking__container--to"
@@ -61,7 +56,6 @@ function SurveyComponent({
       });
     }
   });
-
 
   survey.onValueChanged.add((sender, options) => {
     setTimeout(() => {
@@ -75,7 +69,6 @@ function SurveyComponent({
     }, 0);
   });
 
-
   const dataHardcode = {
     client_id: 533,
     survey_id: 2113,
@@ -84,15 +77,28 @@ function SurveyComponent({
 
   const isLastStep = activeStep === maxSteps - 1;
 
+  const [open, setOpen] = React.useState(false);
+  const modalOpen = () => setOpen(true);
+  const modalClose = () => setOpen(false);
+
   return (
     <>
+      <Button onClick={modalOpen} sx={{ marginLeft: "auto", display: "block"  }}>Instrucciones Encuesta</Button>
+      <Modal open={open} onClose={modalClose} sx={modalStyle}>
+        <Box sx={modalBoxStyle}>
+          <Typography variant="h5" sx={{marginBottom:"10px"}}>Instrucciones</Typography>
+          <Typography variant="body1" sx={{marginBottom:"10px"}}>1.Ordena de Mayor a Menor, segun tu preferencia (Siendo 6 la puntuacion mas alta).</Typography>
+          <Typography variant="body1" sx={{marginBottom:"10px"}}>2.Para ordenar manten pulsada la opcion y arrastrala hasta su posición.</Typography>
+          <Typography variant="body1" sx={{marginBottom:"10px"}}>3.Tienes que marcar las 6 opciones obligatoriamente.</Typography>
+          <Button onClick={modalClose} sx={{marginLeft:"80%"}}>Cerrar</Button>
+        </Box>
+      </Modal>
       <Survey
         model={survey}
         showNavigationButtons="none"
         choicesOrder="one"
         allResults
       />
-
       <MobileStepper
         variant="text"
         steps={maxSteps}
@@ -139,4 +145,18 @@ function SurveyComponent({
   );
 }
 
+const modalStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const modalBoxStyle = {
+  background: "white",
+  padding: "20px",
+  borderRadius: "10px",
+  
+};
+
 export default SurveyComponent;
+
